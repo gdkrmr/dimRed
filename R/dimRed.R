8958a9e867550cb90f18c9347441524c85457863
gdkrmr <gkraemer@bgc-jena.mpg.de>
2016-09-20 11:22:16 +0200

    added funding node, pcainst examples

--- R/dimRed.R
+++ R/dimRed.R
@@ -1,6 +1,10 @@
 #' A package to aggregate dimensionality reduction techniques.
 #'
-#' dimRed collects dimensionality reduction methods that are implemented in R and give them a common interface.
+#' dimRed collects dimensionality reduction methods that are
+#' implemented in R and gives them a common interface.
+#' 
+#' Funding provided by the Max Planck Institute for Biogeochemistry,
+#' Jena, Empirical Inference of the Earth System Group.
 #'
 #' TODO: add a nice description
 "_PACKAGE"
