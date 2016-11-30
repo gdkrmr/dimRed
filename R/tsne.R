c359b47c97c248a672b245d06178d585a28e6f98
gdkrmr <gkraemer@bgc-jena.mpg.de>
2016-11-30 14:57:21 +0100

    fixed but in tsne

--- R/tsne.R
+++ R/tsne.R
@@ -72,7 +72,7 @@ tSNE <- setClass(
         outdata <- Rtsne::Rtsne(pars$d(indata),
                                 perplexity = pars$perplexity,
                                 theta = pars$theta,
-                                ndim = pars$ndim)$Y
+                                ndims = pars$ndim)$Y
 
         colnames(outdata) <- paste0("tSNE", 1:ncol(outdata))
 
