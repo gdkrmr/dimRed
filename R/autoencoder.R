70ecafe8fa0f295017043a8ccf45691d64c2c945
gdkrmr <gkraemer@bgc-jena.mpg.de>
2017-10-15 22:41:40 +0200

    autoencoder code refactoring

--- R/autoencoder.R
+++ R/autoencoder.R
@@ -77,144 +77,174 @@ AutoEncoder <- setClass(
         orgdata <- if (keep.org.data) data@data else NULL
         indata <- data@data
 
-        with(pars, {
-          if (length(n_hidden) != length(activation))
-            stop("declare an activation for each layer")
-          if (length(n_hidden) %% 2 == 0)
-            stop("the number of layers must be impair")
-          if (weight_decay < 0)
-            stop("weight decay must be > 0")
-          if (learning_rate <= 0)
-            stop("learning rate must be > 0")
-          if (n_steps <= 0)
-            stop("n_steps must be > 0")
-          if (ndim != n_hidden[ceiling(length(n_hidden) / 2)])
-            stop("the middle of n_hidden must be equal to ndim")
+        graph <- with(pars, {
+            graph_params(
+                d_in          = ncol(indata),
+                n_hidden      = n_hidden,
+                activation    = activation,
+                weight_decay  = weight_decay,
+                learning_rate = learning_rate,
+                n_steps       = n_steps,
+                ndim          = ndim
+            )
         })
+        graph_data_input <- graph$in_data
+        graph_decoder_input <- graph$in_dec
 
-        tf <- tensorflow::tf
-        n_hidden <- pars$n_hidden
-        activation <- pars$activation
-        n_in <- ncol(indata)
+        optimizer <- tensorflow::tf$train$GradientDescentOptimizer(pars$learning_rate)
+        train <- optimizer$minimize(graph$loss)
 
-        get_activation_function <- function(x) {
-          switch(
-            x,
-            tanh    = tf$tanh,
-            sigmoid = tf$sigmoid,
-            relu    = tf$nn$relu,
-            elu     = tf$elu,
-            I
-          )
-        }
-
-
-        input <- tf$placeholder("float", shape = tensorflow::shape(NULL, n_in),      name = "data")
-        indec <- tf$placeholder("float", shape = tensorflow::shape(NULL, pars$ndim), name = "nlpca")
-
-        w <- lapply(seq_len(length(n_hidden) + 1), function(x) {
-          n1 <- if (x == 1)               n_in else n_hidden[x - 1]
-          n2 <- if (x > length(n_hidden)) n_in else n_hidden[x]
-          tf$Variable(tf$random_uniform(tensorflow::shape(n1, n2), 1.0, -1.0), name = paste0("w_", x))
-        })
-        b <- lapply(seq_len(length(n_hidden) + 1), function (x) {
-          n <- if (x > length(n_hidden)) n_in else n_hidden[x]
-          tf$Variable(tf$zeros(tensorflow::shape(n)), name = paste0("b_", x))
-        })
-
-        enc <- input
-        for (i in 1:ceiling(length(n_hidden) / 2)) {
-          sigma <- get_activation_function(activation[i])
-          enc <- sigma(tf$matmul(enc, w[[i]]) + b[[i]])
-        }
-
-        dec <- indec
-        for (i in (ceiling(length(n_hidden) / 2) + 1):(length(n_hidden) + 1)) {
-          sigma <- get_activation_function(activation[i])
-          dec <- sigma(tf$matmul(dec, w[[i]]) + b[[i]])
-        }
-
-        encdec <- enc
-        for (i in (ceiling(length(n_hidden) / 2) + 1):(length(n_hidden) + 1)) {
-          sigma <- get_activation_function(activation[i])
-          encdec <- sigma(tf$matmul(encdec, w[[i]]) + b[[i]])
-        }
-
-        loss <- Reduce(`+`, lapply(w, function (x) tf$reduce_sum(tf$pow(x, 2))), 0)
-        loss <- Reduce(`+`, lapply(b, function (x) tf$reduce_sum(tf$pow(x, 2))), loss)
-        loss <- tf$reduce_mean((encdec - input) ^ 2) + pars$weight_decay * loss
-        optimizer <- tf$train$GradientDescentOptimizer(pars$learning_rate)
-        train <- optimizer$minimize(loss)
-
-        ## TODO: how does garbage collection work here? will this be around
-        ## forever? or may the graph just disappear from the function
-        ## environments?
-        sess <- tf$Session()
-        sess$run(tf$global_variables_initializer())
-
-        make_feed <- function(data, samples) {
-          tensorflow::dict(
-            input = if (is.na(pars$batchsize))
-                      data
-                    else
-                      data[sample(seq_len(nrow(data)), batchsize), ]
-          )
-        }
+        sess <- tensorflow::tf$Session()
+        ## This closes sess if it is garbage collected.
+        reg.finalizer(sess, function(x) x$close())
+        sess$run(tensorflow::tf$global_variables_initializer())
 
         ## feed <- dict(input = in_data) #[sort(sample(1:nrow(in_data), batchsize)), ])
         ## cat("0 -", sess$run(loss, feed_dict = feed), "\n")
         for (step in 1:pars$n_steps) {
-          sess$run(train, feed_dict = make_feed(indata, pars$batchsize))
-          ## cat(step, "-", sess$run(loss, feed_dict = feed), "\n")
+            sess$run(train, feed_dict =
+                tensorflow::dict(
+                    graph_data_input =
+                        if(is.na(pars$batchsize)) {
+                            indata
+                        } else {
+                            indata[sample(seq_len(nrow(indata)), pars$batchsize), ]
+                        }
+                )
+            )
         }
 
-        outdata <- sess$run(enc, feed_dict = tensorflow::dict(input = indata))
+        outdata <- sess$run(graph$encoder, feed_dict = tensorflow::dict(graph_data_input = indata))
 
         appl <- function(x) {
-          appl.meta <- if (inherits(x, "dimRedData")) x@meta else data.frame()
-          proj <- if (inherits(x, "dimRedData")) x@data else x
+            appl.meta <- if (inherits(x, "dimRedData")) x@meta else data.frame()
+            proj <- if (inherits(x, "dimRedData")) x@data else x
 
-          if (ncol(proj) != ncol(data@data))
-            stop("x must have the same number of dimensions ",
-                 "as the original data")
+            if (ncol(proj) != ncol(data@data))
+                stop("x must have the same number of dimensions ",
+                     "as the original data")
 
-          res <- sess$run(enc, feed_dict = tensorflow::dict(input = proj))
-          colnames(res) <- paste0("nlPCA", seq_len(ncol(res)))
+            res <- sess$run(graph$encoder, feed_dict = tensorflow::dict(graph_data_input = proj))
+            colnames(res) <- paste0("AE", seq_len(ncol(res)))
 
-          new("dimRedData", data = res, meta = appl.meta)
+            new("dimRedData", data = res, meta = appl.meta)
         }
 
         inv <- function(x) {
-          appl.meta <- if (inherits(x, "dimRedData")) x@meta else data.frame()
-          proj <- if (inherits(x, "dimRedData")) x@data else x
+            appl.meta <- if (inherits(x, "dimRedData")) x@meta else data.frame()
+            proj <- if (inherits(x, "dimRedData")) x@data else x
 
-          if (ncol(proj) != pars$ndim)
-            stop("x must have the same number of dimensions ",
-                 "as ndim data")
+            if (ncol(proj) != pars$ndim)
+                stop("x must have the same number of dimensions ",
+                     "as ndim data")
 
-          res <- sess$run(dec, feed_dict = tensorflow::dict(indec = proj))
-          colnames(res) <- colnames(indata)
+            tf$
+            res <- sess$run(graph$decoder, feed_dict = tensorflow::dict(decoder_data_input = proj))
+            colnames(res) <- colnames(indata)
 
-          new("dimRedData", data = res, meta = appl.meta)
+            new("dimRedData", data = res, meta = appl.meta)
         }
 
 
 
-        colnames(outdata) <- paste0("nlPCA", seq_len(ncol(outdata)))
+        colnames(outdata) <- paste0("AE", seq_len(ncol(outdata)))
 
         return(new(
-          "dimRedResult",
-          data = new("dimRedData",
-                     data = outdata,
-                     meta = meta),
-          org.data     = orgdata,
-          apply        = appl,
-          inverse      = inv,
-          has.apply    = TRUE,
-          has.inverse  = TRUE,
-          has.org.data = keep.org.data,
-          method       = "AutoEncoder",
-          pars         = pars
+            "dimRedResult",
+            data = new("dimRedData",
+                       data = outdata,
+                       meta = meta),
+            org.data     = orgdata,
+            apply        = appl,
+            inverse      = inv,
+            has.apply    = TRUE,
+            has.inverse  = TRUE,
+            has.org.data = keep.org.data,
+            method       = "AutoEncoder",
+            pars         = pars
         ))
-        })
+    })
 )
+
+get_activation_function <- function(x) {
+    switch(
+        x,
+        tanh    = tensorflow::tf$tanh,
+        sigmoid = tensorflow::tf$sigmoid,
+        relu    = tensorflow::tf$nn$relu,
+        elu     = tensorflow::tf$elu,
+        I
+    )
+}
+
+graph_params <- function(
+      d_in,
+      n_hidden,
+      activation,
+      weight_decay,
+      learning_rate,
+      n_steps,
+      ndim
+  ) {
+
+    if (length(n_hidden) != length(activation))
+        stop("declare an activation for each layer")
+    if (length(n_hidden) %% 2 == 0)
+        stop("the number of layers must be impair")
+    if (weight_decay < 0)
+        stop("weight decay must be > 0")
+    if (learning_rate <= 0)
+        stop("learning rate must be > 0")
+    if (n_steps <= 0)
+        stop("n_steps must be > 0")
+    if (ndim != n_hidden[ceiling(length(n_hidden) / 2)])
+        stop("the middle of n_hidden must be equal to ndim")
+
+    tf <- tensorflow::tf
+
+    input <- tf$placeholder(
+        "float", shape = tensorflow::shape(NULL, d_in),
+        name = "input"
+    )
+    indec <- tf$placeholder(
+        "float",
+        shape = tensorflow::shape(NULL, ndim),
+        name = "nlpca"
+    )
+
+    w <- lapply(seq_len(length(n_hidden) + 1), function(x) {
+        n1 <- if (x == 1)               d_in else n_hidden[x - 1]
+        n2 <- if (x > length(n_hidden)) d_in else n_hidden[x]
+        tf$Variable(tf$random_uniform(tensorflow::shape(n1, n2), 1.0, -1.0),
+                    name = paste0("w_", x))
+    })
+    b <- lapply(seq_len(length(n_hidden) + 1), function (x) {
+        n <- if (x > length(n_hidden)) d_in else n_hidden[x]
+        tf$Variable(tf$zeros(tensorflow::shape(n)),
+                    name = paste0("b_", x))
+    })
+
+    enc <- input
+    for (i in 1:ceiling(length(n_hidden) / 2)) {
+        sigma <- get_activation_function(activation[i])
+        enc <- sigma(tf$matmul(enc, w[[i]]) + b[[i]])
+    }
+
+    dec <- indec
+    for (i in (ceiling(length(n_hidden) / 2) + 1):(length(n_hidden) + 1)) {
+        sigma <- get_activation_function(activation[i])
+        dec <- sigma(tf$matmul(dec, w[[i]]) + b[[i]])
+    }
+
+    encdec <- enc
+    for (i in (ceiling(length(n_hidden) / 2) + 1):(length(n_hidden) + 1)) {
+        sigma <- get_activation_function(activation[i])
+        encdec <- sigma(tf$matmul(encdec, w[[i]]) + b[[i]])
+    }
+
+    loss <- Reduce(`+`, lapply(w, function (x) tf$reduce_sum(tf$pow(x, 2))), 0)
+    loss <- Reduce(`+`, lapply(b, function (x) tf$reduce_sum(tf$pow(x, 2))), loss)
+    loss <- tf$reduce_mean((encdec - input) ^ 2) + weight_decay * loss
+
+    return(list(encoder = enc, decoder = dec, network = encdec, loss = loss, in_data = input, in_decoder = indec))
+}
--- tests/testthat/test_autoencoder.R
+++ tests/testthat/test_autoencoder.R
@@ -2,10 +2,10 @@
 context("AutoEncoder")
 
 test_that("using the autoencoder", {
-    irisData <- as(iris[, 1:4], "dimRedData")
-    expect_equal(class(irisData)[1], "dimRedData")
+    iris_data <- as(iris[, 1:4], "dimRedData")
+    expect_equal(class(iris_data)[1], "dimRedData")
 
-    ae <- lapply(1:4, function(x) embed(irisData, "AutoEncoder", n_hidden = c(10, x, 10), ndim = x))
+    ae <- lapply(1:4, function(x) embed(iris_data, "AutoEncoder", n_hidden = c(10, x, 10), ndim = x))
     aq <- lapply(ae, function(x) quality(x, "reconstruction_rmse"))
     lapply(ae, function(x) expect_s4_class(x, "dimRedResult"))
 
@@ -15,3 +15,16 @@ test_that("using the autoencoder", {
 
     lapply(1:length(ae), function(x) expect_equal(x, getNDim(ae[[x]])))
 })
+
+
+
+## test_that("garbage collection", {
+##   tmp <- tf$get_session_handle(environment(ae[[1]]@apply)$dec)
+
+##   tmp <- tf$get_default_session()
+
+##   tmp$close
+##   tmp
+##   tf$get_session_handle()
+##   tf$Session()
+## })
