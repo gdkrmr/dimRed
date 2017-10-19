faa6644800d3de0ba998067d11a0060502523adb
gdkrmr <gkraemer@bgc-jena.mpg.de>
2017-10-19 14:53:08 +0200

    some autoencoder documentation

--- R/autoencoder.R
+++ R/autoencoder.R
@@ -23,17 +23,68 @@
 #'   \item{weight_decay}{the coefficient for weight decay,
 #'       set to 0 if no weight decay desired.}
 #'   \item{learning_rate}{The learning rate for gradient descend}
+#'   \item{graph}{Optional: A list of bits and pieces that define the
+#'      autoencoder in tensorflow, see details.}
+#'   \item{keras_graph}{Optional: A list of keras layers that define
+#'      the encoder and decoder, specifying this, will ignore all
+#'      other topology related variables, see details.}
 #'   \item{batchsize}{If NA, all data will be used for training,
 #'       else only a random subset of size batchsize will be used}
 #'   \item{n_steps}{the number of training steps.}
 #' }
 #'
+#' @section Details:
+#'
+#' There are several ways to specify an autoencoder, the simplest is to pass the
+#' number of neurons per layer in \code{n_hidden}, this must be a vector of
+#' integers of impair length and it must be symmetric and the middle number must
+#' be equal to \code{ndim}, For every layer an activation function can be specified
+#' with \code{activation}.
+#'
+#' For regularization weight decay can be specified by setting
+#' \code{weight_decay} > 0.
+#'
+#' Currently only a gradient descent optimizer is used, the learning rate can be
+#' specified by setting \code{learning_rate}.
+#' The learner can operate on batches if \code{batchsize} is not \code{NA}.
+#' The number of steps the learner uses is specified using \code{n_steps}.
+#'
+#' @section Further training a model
+#'
+#' If the model did not converge in the first training phase or training with
+#' different data is desired, the \code{\link{dimRedResult}} object may be
+#' passed as \code{autoencoder} parameter; In this case all topology related
+#' parameters will be ignored.
+#'
+#' @section Using Keras layers
+#'
+#' The encoder and decoder part can be specified using a list of \pkg{keras}
+#' layers. This requires a list with two entries, \code{encoder} should contain
+#' a LIST of keras layers WITHOUT the \link{\code{layer_input}} that will be
+#' concatenated in order to form the encoder part. \code{decoder} should be
+#' defined accordingly, the output of \code{decoder} must have the same number
+#' of dimensions as the input data.
+#'
+#' @section Using Tensorflow
+#'
+#' The model can be entirely defined in \pkg{tensorflow}, it must contain a
+#' list with the following entries:
+#' \describe{
+#'   \item{encoder}{A tensor that defines the encoder.}
+#'   \item{decoder}{A tensor that defines the decoder.}
+#'   \item{network}{A tensor that defines the reconstruction (encoder + decoder).}
+#'   \item{loss}{A tensor that calculates the loss (network + loss function).}
+#'   \item{in_data}{A \code{placeholder} that points to the data input of
+#'     the network AND the encoder.}
+#'   \item{in_decoder}{A \code{placeholder} that points to the input of
+#'     the decoder.}
+#'   \item{session}{A \pkg{tensorflow} \code{Session} object that holds
+#'     the values of the tensors.}
+#' }
+#'
 #' @section Implementation:
 #' Uses \pkg{tensorflow} as a backend, for details an
 #'   problems relating tensorflow, see \url{https://tensorflow.rstudio.com}.
-#' Currently only a very simple Autoencoder is implemented.
-#'
-#' #@references
 #'
 #' @examples
 #' \dontrun{
@@ -109,9 +160,8 @@ AutoEncoder <- setClass(
               tmp <- graph_keras(encoder = pars$keras_graph$encoder,
                                  decoder = pars$keras_graph$decoder,
                                  n_in    = ncol(indata))
-message("ndim = ", pars$ndim)
+
               pars$ndim <- tmp$encoder$shape$dims[[2]]$value
-message("ndim = ", pars$ndim)
 
               tmp
             } else {
