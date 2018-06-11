364302b2d7db8dec30cacf0e3b7bdc133b7bc774
Max Kuhn <mxkuhn@gmail.com>
2018-06-11 10:32:44 -0500

    initial draft of NMF alterations

--- R/nnmf.R
+++ R/nnmf.R
@@ -2,8 +2,8 @@
 #'
 #' S4 Class implementing NNMF.
 #'
-#' NNMF is a method for decomposing a matrix into a smaller 
-#'  dimension such that the constraint that the data (and the 
+#' NNMF is a method for decomposing a matrix into a smaller
+#'  dimension such that the constraint that the data (and the
 #'  projection) are not negative is taken into account.
 #'
 #' @template dimRedMethodSlots
@@ -14,33 +14,43 @@
 #' The method can take the following parameters:
 #' \describe{
 #'   \item{ndim}{The number of output dimensions.}
-#'   \item{method}{character, should be either "scd" for (sequential 
-#'    coordinate-wise descent) or "lee" (for Lee's multiplicative algorithm).}
-#'   \item{loss}{character, should be either  "mse" for mean square error 
-#'    or "mkl" for mean Kullback-Leibler-divergence.}
-#'   \item{max.iter}{integer, maximum number of iterations (default: 500).}
-#'   \item{n.threads}{integer, number of threads (default: no parallelism)}
-#'   \item{verbose}{integer between 0 and 2, for amount of logging. Defaults
-#'    to 0L.}
-#'   \item{rel.tol}{numeric, convergence criterion that defaults to 1e-04.}
+#'   \item{method}{character, which algorithm should be used. See
+#'    \code{\link[NMF]{nmf}} for possible values. Defaults to 
+#'    "brunet"}
+#'   \item{nrun}{integer, the number of times the computations are 
+#'    conducted. See \code{\link[NMF]{nmf}}}
+#'   \item{seed}{integer, a value to control the random numbers used.}   
+#'   \item{options}{named list, other options to pass to  \code{\link[NMF]{nmf}}}
 #' }
 #'
 #' @section Implementation:
 #'
-#' Wraps around \code{\link[NNLM]{nnmf}}. Note that the estimation uses random
+#' Wraps around \code{\link[NMF]{nmf}}. Note that the estimation uses random
 #'  numbers. To create reproducible results, set the random number seed prior
-#'  to execution. 
+#'  to execution.
+#'
+#' @references
+#'
+#' Lee, D.D., Seung, H.S., 1999. Learning the parts of objects by non-negative
+#' matrix factorization. Nature 401, 788-791. https://doi.org/10.1038/44565
 #'
 #' @examples
 #' dat <- loadDataSet("Iris")
-#' 
+#'
 #' set.seed(4646)
 #' factorization <- embed(dat, "NNMF")
-#' 
+#'
 #' proj_dat <- factorization@apply(dat)
 #'
 #' plot(proj_dat@data[, 1], proj_dat@data[, 2])
 #'
+#' # project new values:
+#' 
+#' nn_proj <- predict(emb, iris[1:7, 1:4])
+#' nn_proj
+#' 
+#' inverse(emb, nn_proj)
+#' 
 #' @include dimRedResult-class.R
 #' @include dimRedMethod-class.R
 #' @family dimensionality reduction methods
@@ -50,81 +60,93 @@ NNMF <- setClass(
   "NNMF",
   contains = "dimRedMethod",
   prototype = list(
-    stdpars = list(ndim = 2L,
-                   method = "scd",
-                   loss = "mse",
-                   max.iter = 500L,
-                   n.threads = 1L,
-                   verbose = 0L,
-                   rel.tol = 1e-04),
+    stdpars = list(ndim = 4L,
+                   method = "brunet",
+                   nrun = 3,
+                   seed = sample.int(10^5, 1),
+                   options = list()),
     fun = function (data, pars, keep.org.data = TRUE) {
-      chckpkg("NNLM")
-      
+      chckpkg("NMF")
+
       meta <- data@meta
       orgdata <- if (keep.org.data) data@data else NULL
       data <- data@data
       if (!is.matrix(data))
         data <- as.matrix(data)
-      if (pars$ndim > ncol(data))
+      # NMF expects variables in rows and samples in columns ¯\_(ツ)_/¯
+      data <- t(data)
+      if (pars$ndim > nrow(data))
         stop("`ndim` should be less than the number of columns.",
              call. = FALSE)
       
-      other.data <-
-        NNLM::nnmf(
-          A = data,
-          k = pars$ndim,
-          method = pars$method,
-          loss = pars$loss,
-          max.iter = pars$max.iter,
-          n.threads = pars$n.threads,
-          verbose = pars$verbose,
-          rel.tol = pars$rel.tol
-        )
-      other.data <- 
-        list(W = other.data$W, H = other.data$H, p = ncol(data))
+      args <- list(x = quote(data), rank = pars$ndim, method = pars$method, 
+                   nrun = pars$nrun, seed = pars$seed)
+      
+
+      if(length(pars$options) > 0) 
+        args <- c(args, pars$options)
+
+      nmf_result <- do.call(NMF::nmf, args)
+      
+      # this should work but doesn't 
+      # call <- c(list(quote(NMF::nmf)), args)
+
+      proj <- t(basis(nmf_result))
+      other.data <- list(p = nrow(data))
+      scores <- t(data) %*% t(proj)
+      scores <- as.data.frame(scores)
+      names(scores) <- paste0("NNMF", 1:ncol(scores))
       
       # evaluate results here for functions
-      
-      appl <- function(x) {
 
+      appl <- function(x) {
         appl.meta <- if (inherits(x, "dimRedData")) x@meta else data.frame()
-        proj <- if (inherits(x, "dimRedData")) x@data else x
-        
-        if(!is.matrix(proj))
-          proj <- as.matrix(proj)
-        
-        if (ncol(proj) != other.data$p)
-          stop("x must have the same number of dimensions ",
-               "as the original data (", other.data$p, ")", 
+        dat <- if (inherits(x, "dimRedData")) x@data else x
+
+        if (!is.matrix(dat))
+          dat <- as.matrix(dat)
+
+        if (ncol(dat) != other.data$p)
+          stop("x must have the same number of columns ",
+               "as the original data (", other.data$p, ")",
                call. = FALSE)
-        pars$x <- t(other.data$H)
-        pars$y <- t(proj)
-        pars$ndim <- NULL
-        pars$verbose <- NULL
-        proj <- do.call(NNLM::nnlm, pars)
-        
-        proj <- as.data.frame(t(proj$coefficients))
-        names(proj) <- paste0("NNMF", 1:ncol(proj))
-        
-        proj <- new("dimRedData", data = proj, meta = appl.meta)
+
+        scores <- as.data.frame(dat %*% t(proj))
+        names(scores) <- paste0("NNMF", 1:ncol(scores))
+
+        proj <- new("dimRedData", data = scores, meta = appl.meta)
         return(proj)
+      }
+
+      inv <- function(x) {
+        appl.meta <- if (inherits(x, "dimRedData")) x@meta else data.frame()
+        dat <- if (inherits(x, "dimRedData")) x@data else x
+        
+        if (!is.matrix(dat))
+          dat <- as.matrix(dat)
+
+        reproj <- x %*% proj
+        reproj <- new("dimRedData", data = reproj, meta = appl.meta)
+
+        return(reproj)
       }
 
       res <- new(
         "dimRedResult",
         data         = new("dimRedData",
-                           data = data,
+                           data = scores,
                            meta = meta),
         org.data     = orgdata,
         apply        = appl,
+        inverse      = inv,
         has.org.data = keep.org.data,
         has.apply    = TRUE,
-        has.inverse  = FALSE,
+        has.inverse  = TRUE,
         method       = "NNMF",
         pars         = pars,
         other.data   = other.data
       )
-      
+
       return(res)
     })
 )
