cf903d5a56e00084a157bec0c857b7f570dd9336
gdkrmr <gdkrmr@users.noreply.github.com>
2019-01-28 15:28:14 +0100

    add R-3.5.2 to checkpackage.sh

--- checkpackage.sh
+++ checkpackage.sh
@@ -3,7 +3,8 @@
 echo "== START ============================================="
 
 # R_FOLDER=/usr/bin
-R_FOLDER=$HOME/progs/R/R-3.5.1/bin
+# R_FOLDER=$HOME/progs/R/R-3.5.1/bin
+R_FOLDER=$HOME/progs/R/R-3.5.2/bin
 # R_FOLDER=$HOME/progs/R/R-devel/bin
 
 $R_FOLDER/R --version
