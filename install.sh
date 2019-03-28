6c580970ae90ee8097a6fa6d833a2f7ef8120566
gdkrmr <gdkrmr@users.noreply.github.com>
2019-03-28 17:22:06 +0100

    add new version of r to test script

--- install.sh
+++ install.sh
@@ -5,7 +5,9 @@
 
 # R_FOLDER=/usr/bin
 # R_FOLDER=$HOME/progs/R/R-3.5.1/bin
-R_FOLDER=$HOME/progs/R/R-devel/bin
+# R_FOLDER=$HOME/progs/R/R-3.5.2/bin
+R_FOLDER=$HOME/progs/R/R-3.5.3/bin
+# R_FOLDER=$HOME/progs/R/R-devel/bin
 
 $R_FOLDER/R --version
 
@@ -13,11 +15,11 @@ pkgversion=$(cat DESCRIPTION | grep Version | sed 's|Version: \(.*\)|\1|')
 echo "INSTALLING version $pkgversion"
 $R_FOLDER/R CMD INSTALL dimRed_$pkgversion.tar.gz
 
-$R_FOLDER/R -e 'install.packages(c("NMF", "magrittr", "DRR", "lintr"), Ncpus = 4, ask = FALSE)'
-$R_FOLDER/R -e 'option(Ncpus = 4); dimRed::installSuggests()'
+$R_FOLDER/R -e 'install.packages(c("NMF", "magrittr", "DRR", "lintr"), Ncpus = 4, ask = FALSE, repos = "https://cloud.r-project.org")'
+$R_FOLDER/R -e 'options(Ncpus = 4, repos = "https://cloud.r-project.org"); dimRed::installSuggests()'
 # $R_FOLDER/R -e 'tensorflow::install_tensorflow()'
 # $R_FOLDER/R -e 'keras::install_keras()'
 
 pip install --user --upgrade tensorflow keras umap-learn
 
-$R_FOLDER/R -e 'options(Ncpus = 4); install.packages("devtools")'
+$R_FOLDER/R -e 'options(Ncpus = 4, repos = "https://cloud.r-project.org"); install.packages("devtools")'
