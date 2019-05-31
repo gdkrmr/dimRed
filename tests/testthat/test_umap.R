e590b5d751fea6f015e416da1e717d4e17343184
gdkrmr <gdkrmr@users.noreply.github.com>
2019-05-31 13:49:27 +0200

    fixes umap and adds predict method

--- tests/testthat/test_umap.R
+++ tests/testthat/test_umap.R
@@ -9,21 +9,55 @@ skip_if_no_umap_learn <- function() {
 
 test_that("UMAP python", {
   skip_if_no_umap_learn()
-  res <- embed(iris[1:4], "UMAP", .mute = c("message", "output"))
-  expect_s4_class(res, "dimRedResult")
-  expect_equal(res@method, "UMAP")
-  expect_equal(res@pars$d, "euclidean")
-  expect_equal(res@pars$knn, 15)
-  expect_equal(res@pars$method, "umap-learn")
-  expect_equal(res@pars$ndim, 2)
+
+  res1 <- embed(iris[1:4], "UMAP", .mute = c("message", "output"))
+  res2 <- embed(iris[1:4], "UMAP", .mute = c("message", "output"), knn = 20)
+
+  expect_s4_class(res1, "dimRedResult")
+  expect_equal(res1@method, "UMAP")
+  expect_equal(res1@pars$d, "euclidean")
+  expect_equal(res1@pars$knn, 15)
+  expect_equal(res1@pars$method, "umap-learn")
+  expect_equal(res1@pars$ndim, 2)
+
+  expect_s4_class(res2, "dimRedResult")
+  expect_equal(res2@method, "UMAP")
+  expect_equal(res2@pars$d, "euclidean")
+  expect_equal(res2@pars$knn, 20)
+  expect_equal(res2@pars$method, "umap-learn")
+  expect_equal(res2@pars$ndim, 2)
+
+
+  expect_true(any(res1@data@data != res2@data@data))
+
+  pred1 <- predict(res1, iris[1:4])
+  pred2 <- predict(res1, iris[1:4])
+
+  expect_equal(dim(pred1@data), dim(res1@data@data))
 })
 
 test_that("UMAP R", {
-  res <- embed(iris[1:4], "UMAP", method = "naive", .mute = c("message", "output"))
-  expect_s4_class(res, "dimRedResult")
-  expect_equal(res@method, "UMAP")
-  expect_equal(res@pars$d, "euclidean")
-  expect_equal(res@pars$knn, 15)
-  expect_equal(res@pars$method, "naive")
-  expect_equal(res@pars$ndim, 2)
+  res1 <- embed(iris[1:4], "UMAP", method = "naive", .mute = c("message", "output"))
+  res2 <- embed(iris[1:4], "UMAP", method = "naive", .mute = c("message", "output"), knn = 20)
+
+  expect_s4_class(res1, "dimRedResult")
+  expect_equal(res1@method, "UMAP")
+  expect_equal(res1@pars$d, "euclidean")
+  expect_equal(res1@pars$knn, 15)
+  expect_equal(res1@pars$method, "naive")
+  expect_equal(res1@pars$ndim, 2)
+
+  expect_s4_class(res2, "dimRedResult")
+  expect_equal(res2@method, "UMAP")
+  expect_equal(res2@pars$d, "euclidean")
+  expect_equal(res2@pars$knn, 20)
+  expect_equal(res2@pars$method, "naive")
+  expect_equal(res2@pars$ndim, 2)
+
+  expect_true(any(res1@data@data != res2@data@data))
+
+  pred1 <- predict(res1, iris[1:4])
+  pred2 <- predict(res1, iris[1:4])
+
+  expect_equal(dim(pred1@data), dim(res1@data@data))
 })
