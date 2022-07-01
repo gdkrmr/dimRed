8042505ecbc2667445c9a79d7bf9c853a4aa0860
Guido Kraemer <guido.kraemer@uni-leipzig.de>
2022-07-01 10:18:13 +0200

    fix example

--- R/drr.R
+++ R/drr.R
@@ -66,7 +66,7 @@
 #'
 #' @examples
 #' \dontrun{
-#' if(requireNamespace(c("kernlab", "DRR")), quietly = TRUE) {
+#' if(requireNamespace(c("kernlab", "DRR"), quietly = TRUE)) {
 #'
 #' dat <- loadDataSet("variable Noise Helix", n = 200)[sample(200)]
 #'
