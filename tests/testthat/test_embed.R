239250c251b2fca54d0823b1e6172304e75d66a0
Guido Kraemer <guido.kraemer@uni-leipzig.de>
2025-01-16 14:06:53 +0100

    add tests for argument handling

--- tests/testthat/test_embed.R
+++ tests/testthat/test_embed.R
@@ -4,3 +4,14 @@ test_that("standard method is PCA", {
   expect_equal(res@method, "PCA")
 
 })
+
+test_that("correctly convert .keep.org.data argument", {
+  res1 <- embed(Species ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width,
+        iris, "PCA", .keep.org.data = FALSE)
+
+
+  res2 <- embed(Species ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width,
+        iris, "PCA", .keep.org.data = TRUE)
+
+  expect_equal(as.matrix(iris[1:4]), getOrgData(res2))
+})
